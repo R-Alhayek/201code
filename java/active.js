1a6a9ab79ccffbc1ebea431715cea5bf610a1650
‘R-Alhayek’ <rafeefalhayek94@outlook.com>
2021-04-25 17:58:02 +0300

    save changes

---
+++ java/active.js
@@ -0,0 +1,10 @@
+let name = prompt ('What is your name?');
+  console.log ('name = ', name);
+let country = prompt ('Where are you from?');
+  console.log ('country = ', country);
+let accent = prompt ('Do you prefer American or British accent?');
+  console.log ('accent = ', accent);
+let visit = prompt ('What country do you dream of visiting?');
+  console.log ('visit = ', visit);
+
+alert (`Hello there, ${name} from ${country}. ${accent} accent is not very different from other English accents, it is still English and every place in the world uses english, even ${visit}`);
